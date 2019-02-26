9be2750e6008a552e38d1fbafb392aa398c3ff7f
Jean Prunneaux <jean@prunneaux.com>
2019-02-26 19:14:23 +0100

    New options: quiet mode + stop on error + strict mode

--- testbash.sh
+++ testbash.sh
@@ -6,7 +6,7 @@
 #                                                      #
 #  Author: Jean Prunneaux (http://jean.prunneaux.com)  #
 #                                                      #
-#  Version 2.3.0 (2019-01-30)                          #
+#  Version 2.4.0 (2019-02-26)                          #
 #                                                      #
 ########################################################
 
@@ -27,13 +27,23 @@ tb_dependencies_directory=$tb_current_directory/dependencies
 #  FUNCTIONS  #
 ###############
 
+# Print something if quiet mode disabled
+# Usage: tb_echo TEXT
+tb_echo() {
+	[ "$tb_quietmode" == true ] || echo "$*"
+}
+
+
 # Print script usage
 # Usage: tb_usage
 tb_usage() {
 	echo "Usage: testbash.sh [OPTIONS] [SCRIPT...]"
 	echo "Options:"
 	echo "  -l  Load dependencies before running tests (useful to test a library)"
+	echo "  -s  Stop tests immediately if an error occurs"
+	echo "  -e  Enable strict mode (set -e)"
 	echo "  -d  Run in debug mode (uses bash set -x command)"
+	echo "  -q  Quiet mode"
 	echo "  -h  Print this help"
 	echo
 	echo "Put your unit tests files in the tests/ directory with '.sh' extensions."
@@ -58,6 +68,9 @@ tb_test() {
 	# default values
 	local expected_exitcode=0 expected_result="*" \
 	      test_name interactive=false quiet_mode=false test_value=false
+
+	# global quiet mode: force quiet mode
+	$tb_quietmode && quiet_mode=true
 
 	tb_tests+=1
 
@@ -106,23 +119,16 @@ tb_test() {
 		fi
 	fi
 
-	echo
-	echo "Run unit test for $test_name..."
+	tb_echo
+	tb_echo "Run unit test for $test_name..."
 
-	local result exitcode_ok=false result_ok=false
+	local result exitcode=0 exitcode_ok=false result_ok=false
 
 	# test value mode
 	if $test_value ; then
 		exitcode=$expected_exitcode
 		result=$*
 	else
-		# get command
-		tb_cmd=()
-		while [ -n "$1" ] ; do
-			tb_cmd+=("$1")
-			shift
-		done
-
 		# or test command
 		if $interactive ; then
 			# interactive mode
@@ -132,14 +138,14 @@ tb_test() {
 				$tb_debugmode && set -x
 
 				# run command
-				"${tb_cmd[@]}" &> /dev/null
+				"$@" &> /dev/null || exitcode=$?
 
 			else
 				# enable debugging
 				$tb_debugmode && set -x
 
 				# run command
-				"${tb_cmd[@]}"
+				"$@" || exitcode=$?
 			fi
 		else
 			if $quiet_mode ; then
@@ -147,18 +153,15 @@ tb_test() {
 				$tb_debugmode && set -x
 
 				# run command
-				result=$("${tb_cmd[@]}" &> /dev/null)
+				result=$("$@" 2> /dev/null) || exitcode=$?
 			else
 				# enable debugging
 				$tb_debugmode && set -x
 
 				# run command
-				result=$("${tb_cmd[@]}")
+				result=$("$@") || exitcode=$?
 			fi
 		fi
-
-		# get command result
-		exitcode=$?
 
 		# remove debug mode to avoid unnecessary log
 		$tb_debugmode && set +x
@@ -186,7 +189,7 @@ tb_test() {
 
 	# if test OK
 	if $exitcode_ok && $result_ok ; then
-		echo "...Passed"
+		tb_echo "...Passed"
 
 		# log success
 		tb_success+=("$test_file: $test_name")
@@ -197,7 +200,7 @@ tb_test() {
 		return 0
 
 	else
-		echo "...FAILED"
+		tb_echo "...FAILED"
 
 		# log error details
 		error_details="$test_file: $test_name"
@@ -212,6 +215,9 @@ tb_test() {
 
 		tb_errors+=("$error_details")
 
+		# if stop on error, print final report (and quit)
+		$tb_stoponerror && tb_final_report
+
 		# enable debugging
 		$tb_debugmode && set -x
 
@@ -220,28 +226,84 @@ tb_test() {
 }
 
 
+# Print final report and quit
+# Usage: tb_final_report
+tb_final_report() {
+	tb_echo
+	tb_echo "-----------------------------"
+	tb_echo
+
+	if [ $tb_tests == 0 ] ; then
+		echo "No tests found."
+		tb_usage
+		exit 3
+	fi
+
+	if [ ${#tb_errors[@]} == 0 ] ; then
+		echo "All test succeeded!"
+	else
+		echo "Some tests failed!"
+	fi
+
+	if [ ${#tb_success[@]} -gt 0 ] ; then
+		echo
+		echo "Success: (${#tb_success[@]}/$tb_tests)"
+		for tb_i in "${tb_success[@]}" ; do
+			echo "   - $tb_i"
+		done
+	fi
+	if [ ${#tb_errors[@]} -gt 0 ] ; then
+		echo
+		echo "Errors: (${#tb_errors[@]}/$tb_tests)"
+		for tb_i in "${tb_errors[@]}" ; do
+			echo "   - $tb_i"
+		done
+
+		exit 4
+	fi
+
+	exit
+}
+
+
 ##################
 #  MAIN PROGRAM  #
 ##################
 
 # default options
-tb_debugmode=false
 tb_load_dependencies=false
+tb_stoponerror=false
+tb_debugmode=false
+tb_quietmode=false
 
 # get command line options
-while getopts ":ldh" tb_opts ; do
+while getopts ":lsedqh" tb_opts ; do
 	case $tb_opts in
-		l) tb_load_dependencies=true
-		   ;;
-		d) tb_debugmode=true
-		   ;;
-		h) tb_usage
-		   exit 0
-		   ;;
-		\?) echo "ERROR: '$OPTARG' option does not exist."
-		    tb_usage
-		    exit 1
-		    ;;
+		l)
+			tb_load_dependencies=true
+			;;
+		s)
+			tb_stoponerror=true
+			;;
+		e)
+			tb_stoponerror=true
+			# enable strict mode
+			set -e
+			;;
+		d)
+			tb_debugmode=true
+			;;
+		q)
+			tb_quietmode=true
+			;;
+		h)
+			tb_usage
+			exit 0
+			;;
+		\?)
+			tb_usage
+			exit 1
+			;;
 	esac
 done
 
@@ -297,10 +359,10 @@ echo "Running unit tests..."
 
 # load test files
 for tb_testfile in $(find -L "$tb_tests_directory" -name '*.sh' | sort) ; do
-	echo
-	echo "-----------------------------"
-	echo
-	echo "Running tests from '$(basename $tb_testfile)':"
+	tb_echo
+	tb_echo "-----------------------------"
+	tb_echo
+	tb_echo "Running tests from '$(basename $tb_testfile)':"
 
 	tb_current_test_file=$tb_testfile
 	tb_current_test_directory=$(dirname "$tb_testfile")
@@ -309,52 +371,16 @@ for tb_testfile in $(find -L "$tb_tests_directory" -name '*.sh' | sort) ; do
 	$tb_debugmode && set -x
 
 	# run test file
-	source "$tb_testfile"
-	testfile_res=$?
+	tb_testfile_result=0
+	source "$tb_testfile" || tb_testfile_result=$?
 
 	# remove debug mode to avoid unnecessary log
 	$tb_debugmode && set +x
 
-	if [ $testfile_res != 0 ] ; then
-		tb_errors+=("$tb_testfile returned error ($testfile_res)")
+	if [ $tb_testfile_result != 0 ] ; then
+		tb_errors+=("$tb_testfile returned error ($tb_testfile_result)")
 		break
 	fi
 done
 
-
-##################
-#  FINAL REPORT  #
-##################
-
-echo
-echo "-----------------------------"
-echo
-
-if [ $tb_tests == 0 ] ; then
-	echo "No tests found."
-	tb_usage
-	exit 3
-fi
-
-if [ ${#tb_errors[@]} == 0 ] ; then
-	echo "All test succeeded!"
-else
-	echo "Some tests failed!"
-fi
-
-if [ ${#tb_success[@]} -gt 0 ] ; then
-	echo
-	echo "Success: (${#tb_success[@]}/$tb_tests)"
-	for tb_i in "${tb_success[@]}" ; do
-		echo "   - $tb_i"
-	done
-fi
-if [ ${#tb_errors[@]} -gt 0 ] ; then
-	echo
-	echo "Errors: (${#tb_errors[@]}/$tb_tests)"
-	for tb_i in "${tb_errors[@]}" ; do
-		echo "   - $tb_i"
-	done
-
-	exit 4
-fi
+tb_final_report
